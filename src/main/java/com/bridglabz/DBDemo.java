10c878f42a121a24611ccf4b44a52dd45afcd759
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-23 19:16:16 +0530

    Added: Updated Terisa salary

--- src/main/java/com/bridglabz/DBDemo.java
+++ src/main/java/com/bridglabz/DBDemo.java
@@ -1,6 +1,7 @@
 package com.bridglabz;
 
 import java.sql.*;
+
 public class DBDemo {
     public static void main(String[] args) throws SQLException {
         System.out.println("welcome to employee-payroll mysql");
@@ -10,11 +11,15 @@ public class DBDemo {
         String userName = "root";
 
         String password = "Nsg@1995";
+
         Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(jdbcURL, userName, password);
             Statement statement = connection.createStatement();
+
+            statement.execute("UPDATE employee SET salary=4000000.00 WHERE id=2");
+
             ResultSet resultSet = statement.executeQuery("SELECT *FROM employee ");
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
@@ -22,6 +27,7 @@ public class DBDemo {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
+
             connection.close();
         }
     }
