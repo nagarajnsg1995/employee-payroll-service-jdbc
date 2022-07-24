1b1d6d292fc5d5b57f79effaccec112d0aa4bf43
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-24 11:14:17 +0530

    Added: Reterving particular data from database

--- src/main/java/com/bridglabz/DBDemo.java
+++ src/main/java/com/bridglabz/DBDemo.java
@@ -16,12 +16,13 @@ public class DBDemo {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(jdbcURL, userName, password);
+            Statement statement = connection.createStatement();
 
-            PreparedStatement statement = connection.prepareStatement("UPDATE employee SET salary=5000000.00 WHERE name='Terisa'");
-            statement.executeUpdate();
+//            PreparedStatement statement = connection.prepareStatement("UPDATE employee SET salary=5000000.00 WHERE name='Terisa'");
+//            statement.executeUpdate();
 
 
-             ResultSet resultSet = statement.executeQuery("SELECT *FROM employee ");
+             ResultSet resultSet = statement.executeQuery(" select * from employee where start BETWEEN CAST('2020-02-05' AS DATE) AND DATE(NOW()); ");
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
             }
@@ -33,8 +34,7 @@ public class DBDemo {
         }
     }
 
-    private static class preparedStatement {
-    }
+
 }
 
 
