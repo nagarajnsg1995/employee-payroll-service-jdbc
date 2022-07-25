555a7f0f98cf36250e2514c02e7defd5966e59ed
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-25 21:24:39 +0530

    Added: Deleteing data from database through jdbc

--- src/main/java/com/bridglabz/DBDemo.java
+++ src/main/java/com/bridglabz/DBDemo.java
@@ -19,16 +19,19 @@ public class DBDemo {
             Statement statement = connection.createStatement();
 
 //           PreparedStatement statement = connection.prepareStatement("UPDATE employee SET salary=5000000.00 WHERE name='Terisa'");
+//           statement.execute("insert into employee values (4,'vani','F',2000000,'2022-07-25')");
 
-//           ResultSet resultSet = statement.executeQuery("SELECT SUM(salary) FROM employee WHERE gender = 'M' GROUP BY gender ");
-//            statement.execute("insert into employee values (4,'vani','F',2000000,'2022-07-25')");
-
-
+            statement.execute("DELETE FROM employee WHERE id=1");
             ResultSet resultSet = statement.executeQuery(" SELECT *  FROM employee ");
+//            ResultSet resultSet = statement.executeQuery(" SELECT SUM(salary)  FROM employee ");
+//            ResultSet resultSet = statement.executeQuery(" SELECT avg(salary)  FROM employee ");
+
 
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
-//                System.out.println("Sum of the all  salary " + resultSet.getDouble("avg(salary)"));
+//                System.out.println("Sum of the all  salary " + resultSet.getDouble("SUM(salary)"));
+//                System.out.println("avg of the all  salary " + resultSet.getDouble("avg(salary)"));
+
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
